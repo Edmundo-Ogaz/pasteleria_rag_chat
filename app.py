91d1cdd1ce59f26523bff137900e9a6144fadfd1
Edmundo Ogaz <edmundo.ogaz@gmail.com>
2025-01-24 17:44:57 -0300

    add session_id and initial message

--- app.py
+++ app.py
@@ -1,21 +1,35 @@
 import chainlit as cl
 import requests
+import uuid
+from datetime import datetime
 
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
+@cl.on_chat_start
+async def start():
+    print(uuid.uuid4())
+    session_id = str(uuid.uuid4())
+    # session_id = datetime.today().strftime('%Y%m%d%H%M%S')
+    cl.user_session.set("session_id", session_id)
+    await cl.Message(
+        content="¡Hola! Bienvenidos a Pastelería La Palmera. Este medio es solo para consultas, no se reciben pedidos por este medio. Cualquier pedido que necesite realizar vía telefónica al 55 2 268988 o por nuestra pagina web."
+    ).send()
+
 @cl.on_message
 async def main(message: cl.Message):
-    response = call_backend(message.content)
+    session_id = cl.user_session.get("session_id")
+    response = call_backend(message.content, session_id)
     await cl.Message(
         content = response
     ).send()
 
-def call_backend(user_message):
+def call_backend(user_message, session_id):
     try:
         url = os.environ.get("RAG_MODEL")
-        headers = {"Content-Type": "application/json"}
+        print("url", url)
+        headers = {"Content-Type": "application/json", "sessionId": session_id}
         data = {"query": user_message}
         response = requests.post(url, json=data, headers=headers, timeout=(5, 50))
         response.raise_for_status()
